dfcf2d2b1c6a4b54701cf3273ff8f95170899c01
Avash ghimire <ghimireavash06@gmail.com>
2022-12-04 11:36:15 +0545

    First React App: TextUtils Helps to creat Text and added some featurs in it.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from "./MyComponents/Navbar";
+import TextForm from "./MyComponents/TextForm";
+import About from './MyComponents/About';
+import Alert from "./MyComponents/Alert";
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
+
+// import {Todos} from "./MyComponents/Todos"; 
+// import {Footer} from "./MyComponents/Footer"; 
 function App() {
+  const [mode, setmode] = useState('light');
+  const [redmode, setredmode] = useState('light');
+  const [alert, setAlert] = useState('');
+
+  const displayAlert = (message,type)=>{
+setAlert({
+  msg:message,
+  type:type
+})
+setTimeout(() => {
+  setAlert(null)
+}, 1500);
+  }
+// Enable Blue Dark Mode   
+  const togglemode = () =>{
+    if (mode === 'light') {
+      setmode('dark');
+      document.body.style.backgroundColor='#0c0742' ;
+      displayAlert('Dark mode has been Enable','success')
+    }
+    else{
+        setmode('light'); 
+      document.body.style.backgroundColor='white' ;
+      displayAlert('light mode has been Enable','success')
+
+    }
+  }
+
+  // Enable Red Dark Mode 
+
+  const redtogglemode = () =>{
+    if (redmode === 'light') {
+      setredmode('dark');
+      document.body.style.backgroundColor='#380202' ;
+      displayAlert('Dark mode has been Enable','success')
+    }
+    else{
+        setredmode('light'); 
+      document.body.style.backgroundColor='white' ;
+      displayAlert('light mode has been Enable','success')
+
+    }
+  }
+
+  // let myvalue=33333
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<Router>
+<Navbar title = " TextUtils" aboutTitle = "About" togglemode = {togglemode} mode = {mode} searchBar = {false} redmode={redmode}  redtogglemode={redtogglemode}/>
+<Alert alert = {alert}/>
+<div className='container my-3'>
+<Switch>
+          <Route  exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+<TextForm heading = "Provide your Text" displayAlert={displayAlert} mode={mode}/>
+          </Route>
+ </Switch>
+</div>
+</Router>
+ </>
+
   );
 }
 
---
+++ src/MyComponents/TextForm.js
@@ -0,0 +1,62 @@
+// import React from 'react'
+import React, { useState } from 'react';
+
+export default function TextForm(props) {
+    const handleUpclick = () =>{
+        // console.log("Uppercase was clicked" + text);  
+        let newText = text.toUpperCase();
+        setText(newText); 
+        props.displayAlert("Converted to UpperCase",'success')
+    }
+    const handleDownclick = () =>{
+      // console.log("Lowercase was clicked" + text);  
+      let newText = text.toLowerCase();
+      setText(newText); 
+      props.displayAlert("Converted to LowerCase",'success')
+
+  }
+  const handleClearclick = () =>{
+    // console.log("Uppercase was clicked" + text);  
+    let newText = '';
+    setText(newText); 
+    props.displayAlert("Cleared to",'success')
+
+}
+
+const handleBoldclick = () =>{
+  // console.log("Uppercase was clicked" + text);  
+  let newText = text.b();
+  setText(newText); 
+}
+
+     
+    const handleOnchange = (event) =>{
+        console.log("on change"); 
+        setText (event.target.value); 
+    }
+    const [text, setText] = useState('');
+//  text ("hiiiiiiiiii");
+//  setText ("hello");
+ return (
+  <>
+<div className="container" style={{color:props.mode ==='dark'? 'white':'black'}}>
+<h1>{props.heading} </h1>
+<div className="mb-3">    
+  <textarea className="form-control" value={text}  onChange = {handleOnchange} style ={{backgroundColor:props.mode ==='dark'? '#0f2f5f':'white',color:props.mode ==='dark'? 'white':'black'}} id="mybox" rows="8"></textarea>
+</div>
+<button className="btn btn-primary mx-2" onClick={handleUpclick}>Convert to UpperCase</button>
+<button className="btn btn-primary mx-2" onClick={handleDownclick}>Convert to LowerCase</button>
+<button className="btn btn-primary mx-2" onClick={handleClearclick}>Clear Text</button>
+<button className="btn btn-primary mx-2" onClick={handleBoldclick}>Bold Text</button>
+</div>
+
+<div className='container my3'style={{color:props.mode ==='dark'? 'white':'black'}} >
+  <h2>Your text summary</h2>
+  <p>{text.split(" ").length} words and  {text.length} character</p>
+<p>{0.008 * text.split(" ").length} Minutes read</p>
+<h2>Preview</h2>
+<p>{text}</p>
+</div>
+</>
+  )
+}
